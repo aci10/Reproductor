0108afbb57cd7df6417d92c8a936e46054a980bf
aci10 <aci10crv@gmail.com>
2018-06-25 14:25:11 +0200

    Exportacion a musicxml.

--- app/src/main/java/com/tarareapp_3/reproductor_tarareapp/MusicXML_Writer_t.java
+++ app/src/main/java/com/tarareapp_3/reproductor_tarareapp/MusicXML_Writer_t.java
@@ -40,19 +40,19 @@ public class MusicXML_Writer_t
         a_texto_xml += "<encoding> \n";
 
         if (p_fecha != null && p_fecha.length() > 0)
-            a_texto_xml+= "<encoding-date>" + p_fecha + "</encoding-date>\n";
+            a_texto_xml+= "\t<encoding-date>" + p_fecha + "</encoding-date>\n";
 
-        a_texto_xml += "<encoder>Antonio Candela</encoder> \n" +
-                "<software>Tarareapp</software> \n" +
-                "<encoding-description>Anroid application for music transcription.</encoding-description> \n" +
+        a_texto_xml += "\t<encoder>Antonio Candela</encoder> \n" +
+                "\t<software>Tarareapp</software> \n" +
+                "\t<encoding-description>Anroid application for music transcription.</encoding-description> \n" +
                 "</encoding> \n";
 
         a_texto_xml += "<score-partwise version=\"3.0\"> \n" +
-                        "<part-list> \n" +
-                            "<score-part id=\"P1\"> \n" +
-                                "<part-name>Primera parte</part-name> \n" +
-                            "</score-part> \n" +
-                        "</part-list> \n";
+                        "\t<part-list> \n" +
+                            "\t\t<score-part id=\"P1\"> \n" +
+                                "\t\t\t<part-name>Primera parte</part-name> \n" +
+                            "\t\t</score-part> \n" +
+                        "\t</part-list> \n";
     }
 
     // ---------------------------------------------------------------------------------------------
@@ -97,22 +97,22 @@ public class MusicXML_Writer_t
                     && p_clave_octava > 0
                     && p_pentagramas > 0)
             {
-                a_texto_xml += "<part id=\"P1\"> \n" +
-                                    "<attributes> \n" +
-                                        "<divisions> " + p_bits_compas + " </divisions> \n" +
-                                        "<key> \n" +
-                                            "<fifths> " + p_quintas + " </fifths> \n" +
-                                        "</key> \n" +
-                                        "<time> \n" +
-                                            "<beats> " + p_beats + " </beats> \n" +
-                                            "<beat-type> " + p_beat_type + " </beat-type> \n" +
-                                        "</time> \n" +
-                                        "<clef> \n" +
-                                            "<sign> " + p_clave_nombre + " </sign> \n" +
-                                            "<line> " + p_clave_octava + " </line> \n" +
-                                        "</clef> \n" +
-                                        "<staves> " + p_pentagramas + " </staves> \n" +
-                                    "</attributes> \n";
+                a_texto_xml += "\t<part id=\"P1\"> \n" +
+                                    "\t\t<attributes> \n" +
+                                        "\t\t\t<divisions> " + p_bits_compas + " </divisions> \n" +
+                                        "\t\t\t<key> \n" +
+                                            "\t\t\t\t<fifths> " + p_quintas + " </fifths> \n" +
+                                        "\t\t\t</key> \n" +
+                                        "\t\t\t<time> \n" +
+                                            "\t\t\t\t<beats> " + p_beats + " </beats> \n" +
+                                            "\t\t\t\t<beat-type> " + p_beat_type + " </beat-type> \n" +
+                                        "\t\t\t</time> \n" +
+                                        "\t\t\t<clef> \n" +
+                                            "\t\t\t\t<sign> " + p_clave_nombre + " </sign> \n" +
+                                            "\t\t\t\t<line> " + p_clave_octava + " </line> \n" +
+                                        "\t\t\t</clef> \n" +
+                                        "\t\t\t<staves> " + p_pentagramas + " </staves> \n" +
+                                    "\t\t</attributes> \n";
             }
             else
                 Log.e("Escribe inicio partitura", "atributo incorrecto.");
@@ -127,7 +127,7 @@ public class MusicXML_Writer_t
     {
         if (a_texto_xml != null && a_texto_xml.length() > 0)
         {
-            a_texto_xml += "</part> \n" +
+            a_texto_xml += "\t</part> \n" +
                             "</score-partwise>";
             if (a_escritor != null)
             {
@@ -157,7 +157,7 @@ public class MusicXML_Writer_t
     {
         if (a_texto_xml != null && a_texto_xml.length() > 0)
         {
-            a_texto_xml += "<measure number=\" " + p_numero_compas + " \"> \n";
+            a_texto_xml += "\t\t<measure number=\" " + p_numero_compas + " \"> \n";
         }
         else
             Log.e("Escribe inicio compas", "escritura no inicializada.");
@@ -169,7 +169,7 @@ public class MusicXML_Writer_t
     {
         if (a_texto_xml != null && a_texto_xml.length() > 0)
         {
-            a_texto_xml += "</measure>\n";
+            a_texto_xml += "\t\t</measure>\n";
         }
         else
             Log.e("Escribe cierre compas", "escritura no inicializada.");
@@ -181,20 +181,20 @@ public class MusicXML_Writer_t
     {
         if (a_texto_xml != null && a_texto_xml.length() > 0)
         {
-            a_texto_xml += "<note> \n";
+            a_texto_xml += "\t\t\t<note> \n";
 
             if (p_estado_ligadura > 0)
-                a_texto_xml += "<tie type = \"start\"/> \n";
+                a_texto_xml += "\t\t\t\t<tie type = \"start\"/> \n";
             else if (p_estado_ligadura < 0)
-                a_texto_xml += "<tie type = \"stop\"/> \n";
+                a_texto_xml += "\t\t\t\t<tie type = \"stop\"/> \n";
 
-            a_texto_xml += "<pitch> \n" +
-                                "<step> " + p_nombre + " </step> \n" +
-                                "<octave> " + p_octava + " </octave> \n" +
-                            "</pitch> \n" +
-                            "<duration> " + p_duracion + " </duration> \n" +
-                            "<type> whole </type> \n" +
-                        "</note> \n";
+            a_texto_xml += "\t\t\t\t<pitch> \n" +
+                                "\t\t\t\t\t<step> " + p_nombre + " </step> \n" +
+                                "\t\t\t\t\t<octave> " + p_octava + " </octave> \n" +
+                            "\t\t\t\t</pitch> \n" +
+                            "\t\t\t\t<duration> " + p_duracion + " </duration> \n" +
+                            "\t\t\t\t<type> whole </type> \n" +
+                        "\t\t\t</note> \n";
         }
         else
             Log.e("Escribe escribe nota", "escritura no inicializada.");
@@ -206,10 +206,10 @@ public class MusicXML_Writer_t
     {
         if (a_texto_xml != null && a_texto_xml.length() > 0)
         {
-            a_texto_xml += "</note>\n" +
-                                "<rest measure=\"yes\"/> \n" +
-                                "<duration> " + p_duracion + " </duration> \n" +
-                            "<note> \n";
+            a_texto_xml += "\t\t\t</note>\n" +
+                                "\t\t\t\t<rest measure=\"yes\"/> \n" +
+                                "\t\t\t\t<duration> " + p_duracion + " </duration> \n" +
+                            "\t\t\t<note> \n";
         }
         else
             Log.e("Escribe escribe silencio", "escritura no inicializada.");
